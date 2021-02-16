2f3e551da283da585228d3c0542a56edd61165fc
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-02-16 00:28:45 -0600

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
-
+//Lastly, import Provide from redu and wrap it the App.js so that the entire application can access redux store
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={createStore(reducers)}>
