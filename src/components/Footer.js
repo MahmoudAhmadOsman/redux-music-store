a670a856505f0ca6b2f1153df1be5df516a5d23f
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-02-16 23:16:43 -0600

    added song details component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,10 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col">
-            <p>&copy; Copyright 2021 . All Rights Reserved. Mahmoud Osman</p>
+            <p>
+              &copy; Copyright {new Date().getFullYear()} . All Rights Reserved.
+              Mahmoud Osman
+            </p>
           </div>
         </div>
       </div>
